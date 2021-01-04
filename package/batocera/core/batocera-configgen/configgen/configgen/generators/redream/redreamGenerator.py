481fb84fd14209e6e103def582993de2a9c54789
liberodark <liberodark@gmail.com>
2021-01-04 21:30:13 +0100

    add redream

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/redream/redreamGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/redream/redreamGenerator.py
@@ -3,23 +3,15 @@
 import Command
 from generators.Generator import Generator
 import controllersConfig
-from shutil import copyfile
-import os
+
 
 class RedreamGenerator(Generator):
 
     def generate(self, system, rom, playersControllers, gameResolution):
-
-        configdir = "/userdata/system/configs/redream"
-        if not os.path.exists(configdir):
-            os.makedirs(configdir)
-
-        copyfile("/usr/bin/redream", configdir + "/redream")
-        os.chmod(configdir + "/redream", 0775)
-
-        commandArray = [configdir + "/redream", rom]
+        commandArray = ["redream", rom]
         return Command.Command(
             array=commandArray,
             env={
                 'SDL_GAMECONTROLLERCONFIG': controllersConfig.generateSdlGameControllerConfig(playersControllers)
+                'HOME': "/userdata/system/configs/redream" # to force redream to read/write config file here
             })
